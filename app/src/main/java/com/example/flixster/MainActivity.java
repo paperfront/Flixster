edb8862eb3f67dda45f441c5ccd1032a735154ae
paperfront <mattlacayo@berkeley.edu>
2020-06-24 10:37:25 -0700

    setup movies API requests

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    public static final String NOW_PLAYING_URL = getURL();
+
+    List<Movie> movies;
+
+    // Method to get the api key URL
+    private static String getURL() {
+        return "https://api.themoviedb.org/3/movie/now_playing?api_key=" + getString(R.string.movies_api_key);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "API Request Succeeded");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies = Movie.fromJsonArray(results);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception");
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "API Request failed: " + response);
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,57 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    private static String BASE_URL = "https://image.tmdb.org/t/p/w342";
+
+    private double popularity;
+    private String posterPath;
+    private String title;
+    private String overview;
+
+    // Constructor for Movie objects
+    public Movie(JSONObject jsonObject) throws JSONException {
+
+        popularity = jsonObject.getDouble("popularity");
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+
+    }
+
+
+    // Loads movies from a JSON array
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+
+    // Getters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public String getPosterPath() {
+        return BASE_URL + posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
