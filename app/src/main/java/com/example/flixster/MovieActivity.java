77bee7cfe02e22cb4608fd2070ab6c54504017d1
paperfront <mattlacayo@berkeley.edu>
2020-06-25 14:35:59 -0700

    Added youtube api

--- app/src/main/java/com/example/flixster/MovieActivity.java
+++ app/src/main/java/com/example/flixster/MovieActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.target.Target;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -40,12 +42,15 @@ public class MovieActivity extends AppCompatActivity {
     public static final String TAG = "MovieActivity";
 
     ImageView ivBanner;
+    ImageView ivPlayButton;
     TextView tvTitle;
     TextView tvOverview;
     TextView tvPopularity;
     RatingBar ratingMovie;
 
     List<Movie> movies;
+
+    String movieKey = "NONE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +64,14 @@ public class MovieActivity extends AppCompatActivity {
         tvOverview = binding.tvOverview;
         tvPopularity = binding.tvPopularity;
         ratingMovie = binding.ratingMovie;
+        ivPlayButton = binding.ivPlayButton;
 
-        Movie movie = (Movie) getIntent().getSerializableExtra(MainActivity.KEY_MOVIE);
+        final Movie movie = (Movie) getIntent().getSerializableExtra(MainActivity.KEY_MOVIE);
         ratingMovie.setRating((float) movie.getRating() / 2);
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
         tvPopularity.setText("Popularity Score: " + Double.toString(movie.getPopularity()) + "%");
+        loadTrailer(movie);
 
         String imageUrl;
         RoundedCornersTransformation transformation;
@@ -80,7 +87,6 @@ public class MovieActivity extends AppCompatActivity {
             transformation = new RoundedCornersTransformation(30, 0);
         }
         Glide.with(this).load(imageUrl).transform(transformation).override(Target.SIZE_ORIGINAL).into(ivBanner);
-
         //Create on click listener
         FeaturedAdapter.OnClickListener onClickListener = new FeaturedAdapter.OnClickListener() {
             @Override
@@ -101,6 +107,53 @@ public class MovieActivity extends AppCompatActivity {
         rvMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         performQuery(movieAdapter, movie);
 
+        ivBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchTrailer();
+            }
+        });
+
+    }
+
+    private void launchTrailer() {
+        if (movieKey.equals("NONE")) {
+            return;
+        } else {
+            Intent i = new Intent(MovieActivity.this, MovieTrailerActivity.class);
+            i.putExtra("key", movieKey);
+            startActivity(i);
+        }
+    }
+
+    private void loadTrailer(Movie movie) {
+        String query = "https://api.themoviedb.org/3/movie/%s/videos?api_key=%s&language=en-US";
+        query = String.format(query, movie.getId(), getString(R.string.movies_api_key));
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(query, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "API Request Succeeded");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    JSONObject data = results.getJSONObject(0);
+                    if (!data.getString("site").equals("YouTube")) {
+                        return;
+                    }
+                    movieKey = data.getString("key");
+                    ivPlayButton.setVisibility(View.VISIBLE);
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "API Request failed: " + response);
+            }
+        });
     }
 
     private void performQuery(final FeaturedAdapter adapter, Movie movie) {
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -71,6 +71,7 @@ public class Movie implements Serializable {
         return rating;
     }
 
+
     public int getId() {
         return id;
     }
